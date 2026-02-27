8d37c489bda7479c2defdc1195430f2f7a3fa261
Rachel Li <lirachel@mit.edu>
2026-02-27 16:19:17 -0500

    Update completed shift_add_multiplier_bv.v

--- shift_add_multiplier/shift_add_multiplier_bv.v
+++ shift_add_multiplier/shift_add_multiplier_bv.v
@@ -25,8 +25,7 @@ Proof.
   f_equal.
   - rewrite bv_wrap_small.
     split.
-    * bv_saturate.
-    * bv_saturate.
+    * bv_saturate. rewrite Hrange. Search bv_modulus.
       unfold bv_modulus.
       apply (Z.lt_trans _ (Z.of_N n)); [exact Hrange|].
       apply Z.log2_lt_pow2; [lia|].
@@ -257,10 +256,76 @@ Proof.
       * cbn. rewrite Z_to_bv_unsigned. rewrite bv_wrap_small. reflexivity.
         unfold partial_mul. unfold bv_modulus.
         assert (0 <= bv_unsigned b `mod` 2 ^ bv_unsigned (i st + bv_one) < 2 ^ bv_unsigned (i st + bv_one)).
-        Check Z.pow_add_r.
-        Search ((_ `mod` _) < _).
-        admit.
-Admitted.
+        Search (_ `mod` ?a < ?a).
+        apply Z.mod_pos_bound. apply Z.pow_pos_nonneg. lia. destruct H2.
+        rewrite H2. unfold bv_one. rewrite bv_add_unsigned. unfold bv_wrap. unfold bv_modulus.
+        set (u := bv_unsigned (i st)).
+        set (M := 2 ^ Z.of_N n).
+        assert (Hone : bv_unsigned (Z_to_bv n 1) = 1).
+        {
+          apply bv_unsigned_Z_to_bv.
+          split; [lia|].
+          subst M.
+          pose proof (n_gt_0_pow_gt_1 n pf_n_gt_zero) as Hgt1.
+          lia.
+        }
+
+        rewrite Hone.
+        subst u M.
+        assert (Hrange :
+          0 <= bv_unsigned (i st) + 1 < 2 ^ Z.of_N n).
+        {
+          split; [lia|].
+          assert (Hn_lt_pow : Z.of_N n < 2 ^ Z.of_N n).
+          { apply Z.pow_gt_lin_r; lia. }
+          lia.
+        }
+        rewrite (Z.mod_small (bv_unsigned (i st) + 1) (2 ^ Z.of_N n)) by exact Hrange.
+        lia.
+        
+        set (A := bv_unsigned a).
+        set (M := bv_unsigned b `mod` 2 ^ bv_unsigned (i st + bv_one)).
+
+        pose proof (bv_unsigned_in_range n a) as HA.
+        pose proof (bv_unsigned_in_range n b) as HB.
+
+        (* split Goal: lower and upper *)
+        split.
+        apply Z.mul_nonneg_nonneg. subst A; exact (proj1 HA). subst M; exact (proj1 H5).
+
+        assert (HMle : M <= bv_unsigned b).
+        {
+          subst M.
+          apply Z.mod_le.
+          - exact (proj1 HB).
+          - apply Z.pow_pos_nonneg. lia. unfold bv_one. rewrite bv_add_unsigned. unfold bv_wrap. lia.
+        }
+
+        assert (HAm_le : A * M <= A * bv_unsigned b).
+        {
+          apply Z.mul_le_mono_nonneg_l.
+          - subst A; exact (proj1 HA).
+          - exact HMle.
+        }
+
+        assert (HAb_lt : A * bv_unsigned b < bv_modulus n * bv_modulus n).
+        {
+          eapply Z.mul_lt_mono_nonneg.
+          - subst A; exact (proj1 HA).
+          - subst A. lia.
+          - lia.
+          - exact (proj2 HB).
+        }
+
+        assert (HAm_lt : A * M < bv_modulus n * bv_modulus n).
+        { eapply Z.le_lt_trans; [exact HAm_le | exact HAb_lt]. }
+        unfold bv_modulus in HAm_lt.
+        unfold bv_modulus.
+
+        rewrite N2Z.inj_add.
+        rewrite Z.pow_add_r by lia.
+        exact HAm_lt.
+Qed.
 
 Print iter.
 Lemma iter_commute:
@@ -285,162 +350,21 @@ Proof.
   {
     unfold stn. unfold st0. simpl. unfold init_multiplier.
     apply CaseBusy; simpl; auto.
-    * admit.
+    * apply bv_eq. rewrite bv_shiftr_unsigned. rewrite Z.shiftr_0_r. reflexivity.
     * rewrite bv_unsigned_Z_to_bv; lia.
-    * rewrite bv_unsigned_Z_to_bv; [| lia]. unfold partial_mul.
-      admit. (* partial mul proof*)
+    * rewrite bv_unsigned_Z_to_bv; [| lia]. unfold partial_mul. rewrite Z_to_bv_unsigned.
+      unfold bv_wrap. rewrite Zmod_0_l. rewrite Z.pow_0_r. rewrite Z.mod_1_r. lia.
   }
   unfold stn. unfold st0. simpl. apply shift_and_add_step_correct in IHm.
   unfold stn in *. unfold st0 in *.
-  Print iter.
-
-
-
-
-
-(* Proof that the shift-add process terminates in exactly n iterations
-Lemma shift_and_add_constant_time :
-  forall (a b : bv n),
-  let st0 := init_multiplier a b in
-
-  let stn m := iter (N.to_nat m) shift_and_add_step st0 in
-  forall (m : N),
-  ((m >= n)%N -> is_done (stn m) = true /\
-  bv_unsigned (stn m).(C) = bv_unsigned a * bv_unsigned b) /\
-  ((m < n)%N -> is_done (stn m) = false).
-Proof.
-  intros a b.
-  
-  (* Helper lemma: after k iterations (k < n), i = k and is_done = false *)
-  assert (Hhelper: forall k : nat,
-    (k < N.to_nat n)%nat ->
-    bv_unsigned (i (iter k shift_and_add_step (init_multiplier a b))) = Z.of_nat k /\
-    is_done (iter k shift_and_add_step (init_multiplier a b)) = false).
-  {
-    induction k as [|k IH].
-    - (* Base case: k = 0 *)
-      intros Hlt. simpl.
-      unfold init_multiplier. simpl.
-      split.
-      + rewrite bv_unsigned_Z_to_bv; [reflexivity|].
-        split; [lia|].
-        unfold bv_modulus. 
-
-        assert (Z.of_N n >= 1)%Z by (apply Z_of_N_pos; exact pf_n_gt_zero).
-        assert (2 ^ 1 <= 2 ^ Z.of_N n)%Z by (apply Z.pow_le_mono_r; lia).
-        assert (2 ^ 1 = 2)%Z by reflexivity.
-        rewrite H1 in H0.
-        lia.
-      + reflexivity.
-    - (* inductive case: k -> S k *)
-      intros Hlt.
-      simpl iter.
-      destruct (iter k shift_and_add_step (init_multiplier a b)) as [A' B' C' i' done'] eqn:Hiter.
-      simpl.
-      assert (Hk: (k < N.to_nat n)%nat) by lia.
-      specialize (IH Hk).
-      
-      (* extract information from IH using Hiter *)
-      assert (Hi' : bv_unsigned i' = Z.of_nat k).
-      {
-        assert (H_from_IH := proj1 IH).
-        pattern (iter k shift_and_add_step (init_multiplier a b)) in H_from_IH.
-        rewrite Hiter in H_from_IH.
-        exact H_from_IH.
-      }
-      assert (Hdone' : done' = false).
-      {
-        assert (H_from_IH := proj2 IH).
-        pattern (iter k shift_and_add_step (init_multiplier a b)) in H_from_IH.
-        rewrite Hiter in H_from_IH.
-        simpl in H_from_IH.
-        exact H_from_IH.
-      }
-      
-      (* rewrite iter k ... with concrete state in the goal *)
-      assert (H_step_eq : shift_and_add_step (iter k shift_and_add_step (init_multiplier a b)) = 
-                          shift_and_add_step {| OperandA := A'; OperandB := B'; C := C'; i := i'; is_done := done' |}).
-      { f_equal. exact Hiter. }
-      pattern (shift_and_add_step (iter k shift_and_add_step (init_multiplier a b))).
-      rewrite H_step_eq.
-      unfold shift_and_add_step.
-      simpl.
-      destruct done' as [|] eqn:H_done_eq.
-      * rewrite Hdone' in H_done_eq. discriminate.
-      * simpl.
-        split.
-        ** admit.
-        ** destruct (decide (i' = Z_to_bv n (Z.of_N (n - 1)))).
-           *** apply 
-           exfalso.
-               apply bv_eq in e. simpl in *. subst.
-               rewrite bv_unsigned_Z_to_bv in e; [|split; [lia|unfold bv_modulus; apply Z.pow_gt_lin_r; [lia|apply pf_n_gt_zero]]].
-               rewrite Hi' in e.
-               lia.
-           *** reflexivity.
-  }
-  
-  (* use helper to prove after n-1 iterations, i = n-1 and is_done = false *)
-  (* after one more step -> is_done = true *)
-  assert (Hn1: (N.to_nat n - 1 < N.to_nat n)%nat) by lia.
-  specialize (Hhelper (N.to_nat n - 1) Hn1).
-  simpl in Hhelper.
-  destruct Hhelper as [Hi_n1 Hdone_n1].
-  
-  (* show that after n-1 iterations, i = Z_to_bv n (Z.of_N (n-1)) *)
-  assert (Hi_eq: i (iter (N.to_nat n - 1) shift_and_add_step (init_multiplier a b)) = 
-                 Z_to_bv n (Z.of_N (n - 1))).
-  {
-    apply bv_eq.
-    rewrite bv_unsigned_Z_to_bv; [|split; [lia|unfold bv_modulus; apply Z.pow_gt_lin_r; [lia|apply pf_n_gt_zero]]].
-    exact Hi_n1.
-  }
-  
-  (* prove after n iterations, is_done = true *)
-  simpl iter.
-  rewrite <- Nat.add_1_r.
-  simpl.
-  unfold shift_and_add_step.
-  simpl.
-  rewrite Hdone_n1.
-  simpl.
-  rewrite Hi_eq.
-  destruct (decide (Z_to_bv n (Z.of_N (n - 1)) = Z_to_bv n (Z.of_N (n - 1)))).
-  - reflexivity.
-  - exfalso. apply n0. reflexivity.
-Qed. *)
+  rewrite <- (iter_commute Multiplier m shift_and_add_step (init_multiplier a b)).
+  exact IHm.
+Qed.
 
 End WithContext.
+
 
 Compute shift_and_add_multiplier 4 (Z_to_bv 4 13) (Z_to_bv 4 9).
 Compute shift_and_add_multiplier 3 (Z_to_bv 4 13) (Z_to_bv 4 9).
 Compute shift_and_add_multiplier 5 (Z_to_bv 4 13) (Z_to_bv 4 9).
 Compute shift_and_add_multiplier 10 (Z_to_bv 4 13) (Z_to_bv 4 9).
-
-
-(* Correctness *)
-
-(* Lemma bv_shiftr_correct (n : N) (a : bv n) :
-  (2 * (bv_unsigned (bv_shiftr a bv_one)))%Z = (bv_unsigned a)%Z.
-Proof.
-Admitted. *)
-
-(* Proof:
-     The algorithm maintains the invariant that after k iterations:
-     - C = (sum of A * 2^i for each bit i of B examined so far) * 2^(n-k)
-     - The remaining bits of B are in OperandB
-     
-     After n iterations:
-     - All bits of B have been examined
-     - C contains the full product
-     
-     lemmas:
-     - bv_shiftr (divides by 2)
-     - bv_shiftl (multiplies by 2)
-     - bv_add correctness
-     - bv_extract for LSB extraction
-     - bv_zero_extend correctness
-  *)
-
-
-Compute shift_and_add_multiplier 4 (Z_to_bv 4 13) (Z_to_bv 4 9).
